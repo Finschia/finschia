72ff71c82595a1e399564d87b8f7c890ca8d9e79
Jiyong Ha <jiyong.ha@linecorp.com>
2022-03-03 16:55:29 +0900

    build: change the wasmvm directory for static build

--- builders/scripts/build-static.sh
+++ builders/scripts/build-static.sh
@@ -7,6 +7,7 @@ go mod download
 
 # build wasmvm static
 cd "$(go list -f "{{ .Dir }}" -m github.com/line/wasmvm)" || exit 1
+cd ./libwasmvm
 RUSTFLAGS='-C target-feature=-crt-static' cargo build --release --example staticlib
 mv -f target/release/examples/libstaticlib.a /usr/lib/libwasmvm_static.a
 rm -rf target

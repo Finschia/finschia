683345922748f1215ec6f14b6ba5412c73accd08
TAKAMI Torao <koiroha@gmail.com>
2023-02-21 18:31:50 +0900

    fix changelog, remove echo debugging

--- builders/scripts/install-libwasmvm.sh
+++ builders/scripts/install-libwasmvm.sh
@@ -6,13 +6,11 @@
 set -e
 
 WASMVM_VERSION=`go list -m github.com/line/wasmvm | awk '{print $2}' | grep -o 'v\d\+\.\d\+\.\d\+-\d\+\.\d\+\.\d\+'`
-echo $WASMVM_VERSION
 curl -L -f -o ./checksums.txt https://github.com/line/wasmvm/releases/download/${WASMVM_VERSION}/checksums.txt
 for arch in x86_64 aarch64
 do
   curl -L -f -o /lib/libwasmvm_muslc.${arch}.a  https://github.com/line/wasmvm/releases/download/${WASMVM_VERSION}/libwasmvm_muslc.$arch.a
   CHECKSUM=`grep libwasmvm_muslc.${arch}.a ./checksums.txt | awk '{print $1}'`
-  echo "${CHECKSUM}"
   if [ -z "${CHECKSUM}" ]
   then
     echo "ERROR: libwasmvm_muslc.${arch}.a"
